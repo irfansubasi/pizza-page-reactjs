0b56e6b9195ddb3b388c394e49247c95c5417b80
irfansubasi <136987051+irfansubasi@users.noreply.github.com>
2024-10-06 19:20:40 +0300

    add MiniHeader component

---
+++ src/Components/MiniHeader/MiniHeader.jsx
@@ -0,0 +1,14 @@
+export default function MiniHeader() {
+  return (
+    <header className="header-section">
+      <div className="header-content formpage-container">
+        <img src="/Assets/Iteration-1-assets/logo.svg" alt="logo" />
+        <nav>
+          <a href="#">Anasayfa</a>
+          <span> - </span>
+          <a href="#">Sipariş Oluştur</a>
+        </nav>
+      </div>
+    </header>
+  );
+}
