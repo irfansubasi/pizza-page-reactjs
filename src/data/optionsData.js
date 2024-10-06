113979c183e63d729e6da915bbbba2fc8cf8e83e
irfansubasi <136987051+irfansubasi@users.noreply.github.com>
2024-10-06 19:03:53 +0300

    add datas

---
+++ src/data/optionsData.js
@@ -0,0 +1,20 @@
+export const sizes = ['Küçük', 'Orta', 'Büyük'];
+
+export const doughTypes = ['Kalın', 'Orta', 'İnce'];
+
+export const toppings = [
+  { name: 'Pepperoni' },
+  { name: 'Tavuk Izgara' },
+  { name: 'Mısır' },
+  { name: 'Sarımsak' },
+  { name: 'Ananas' }, //war crime about pizza(I personally don't support this)
+  { name: 'Sosis' },
+  { name: 'Soğan' },
+  { name: 'Sucuk' },
+  { name: 'Biber' },
+  { name: 'Kabak' },
+  { name: 'Kanada Jambonu' },
+  { name: 'Domates' },
+  { name: 'Jalepeno' },
+  { name: 'Sucuk' },
+];
---
+++ src/data/priceData.js
@@ -0,0 +1,21 @@
+const foodPrices = {
+  TerminalPizza: 110,
+  AbsolutePizza: 130,
+  ChickenBurger: 100,
+};
+
+const pizzaSizePrices = {
+  Small: 0, //The price shown on the menu is for small size
+  Medium: 50,
+  Large: 60,
+};
+
+const doughTypePrices = {
+  Thin: 20,
+  Regular: 0, // No extra charge for regular crust
+  Thick: 25,
+};
+
+const ingredientPrice = 10;
+
+export { foodPrices, pizzaSizePrices, doughTypePrices, ingredientPrice };
