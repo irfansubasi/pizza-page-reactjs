f316cad47ecfaeae4c70b09ce411488b429fe5fb
irfansubasi <136987051+irfansubasi@users.noreply.github.com>
2024-10-07 22:12:52 +0300

    add route map

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import OrderPage from './Pages/OrderPage/OrderPage';
+import SuccessPage from './Pages/SuccessPage/SuccessPage';
+import MainHeader from './Components/MainHeader/MainHeader';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<MainHeader />} />
+      <Route path="/order" element={<OrderPage />} />
+      <Route path="/success" element={<SuccessPage />} />
+    </Routes>
+  );
 }
 
 export default App;
