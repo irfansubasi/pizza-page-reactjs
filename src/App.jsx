df51c00fb2781906d0eceb2dd92eb8b225dabebb
irfansubasi <136987051+irfansubasi@users.noreply.github.com>
2024-10-08 17:07:30 +0300

    add navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import OrderPage from './Pages/OrderPage/OrderPage';
 import SuccessPage from './Pages/SuccessPage/SuccessPage';
 import MainHeader from './Components/MainHeader/MainHeader';
 import { Route, Routes } from 'react-router-dom';
+import HomePage from './Pages/HomePage/HomePage';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<MainHeader />} />
+      <Route path="/" element={<HomePage />} />
       <Route path="/order" element={<OrderPage />} />
       <Route path="/success" element={<SuccessPage />} />
     </Routes>
