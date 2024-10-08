65f75f4c4701d3d52eeb14af16c0ac704b1d208b
irfansubasi <136987051+irfansubasi@users.noreply.github.com>
2024-10-08 18:49:58 +0300

    fix navbar bg color

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,78 +1,82 @@
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-export default function Navbar() {
-  const items = [
-    {
-      id: 1,
-      src: `../../../Assets/Iteration-2-aseets/icons/1.svg`,
-      alt: 'kore',
-      text: 'YENİ! Kore',
-    },
-    {
-      id: 2,
-      src: '../../../Assets/Iteration-2-aseets/icons/2.svg',
-      alt: 'pizza',
-      text: 'Pizza',
-    },
-    {
-      id: 3,
-      src: '../../../Assets/Iteration-2-aseets/icons/3.svg',
-      alt: 'burger',
-      text: 'Burger',
-    },
-    {
-      id: 4,
-      src: '../../../Assets/Iteration-2-aseets/icons/4.svg',
-      alt: 'kızartma',
-      text: 'Kızartmalar',
-    },
-    {
-      id: 5,
-      src: '../../../Assets/Iteration-2-aseets/icons/5.svg',
-      alt: 'fastfood',
-      text: 'Fast Food',
-    },
-    {
-      id: 6,
-      src: '../../../Assets/Iteration-2-aseets/icons/6.svg',
-      alt: 'içecek',
-      text: 'Gazlı İçecek',
-    },
-  ];
+const Nav = styled.nav`
+  background-color: var(--white-color);
+`;
 
-  const MenuItems = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-    padding: 1.2rem 0;
-  `;
+const MenuItems = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  padding: 1.2rem 0;
+`;
 
-  const StyledLink = styled(Link)`
-    text-align: center;
-    font-family: 'Barlow', system-ui;
-    font-weight: 500;
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    gap: 0.5rem;
-    color: var(--grey-dark-color);
+const StyledLink = styled(Link)`
+  text-align: center;
+  font-family: 'Barlow', system-ui;
+  font-weight: 500;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  gap: 0.5rem;
+  color: var(--grey-dark-color);
+  padding: 0.5rem 1.3rem;
+
+  &:hover {
+    background: var(--grey-dark-color);
+    color: var(--white-color);
     padding: 0.5rem 1.3rem;
+    border-radius: 3rem;
+  }
+`;
 
-    &:hover {
-      background: var(--grey-dark-color);
-      color: var(--white-color);
-      padding: 0.5rem 1.3rem;
-      border-radius: 3rem;
-    }
-  `;
+const StyledImg = styled.img`
+  width: 2.5rem;
+`;
 
-  const StyledImg = styled.img`
-    width: 2.5rem;
-  `;
+const items = [
+  {
+    id: 1,
+    src: `../../../Assets/Iteration-2-aseets/icons/1.svg`,
+    alt: 'kore',
+    text: 'YENİ! Kore',
+  },
+  {
+    id: 2,
+    src: '../../../Assets/Iteration-2-aseets/icons/2.svg',
+    alt: 'pizza',
+    text: 'Pizza',
+  },
+  {
+    id: 3,
+    src: '../../../Assets/Iteration-2-aseets/icons/3.svg',
+    alt: 'burger',
+    text: 'Burger',
+  },
+  {
+    id: 4,
+    src: '../../../Assets/Iteration-2-aseets/icons/4.svg',
+    alt: 'kızartma',
+    text: 'Kızartmalar',
+  },
+  {
+    id: 5,
+    src: '../../../Assets/Iteration-2-aseets/icons/5.svg',
+    alt: 'fastfood',
+    text: 'Fast Food',
+  },
+  {
+    id: 6,
+    src: '../../../Assets/Iteration-2-aseets/icons/6.svg',
+    alt: 'içecek',
+    text: 'Gazlı İçecek',
+  },
+];
 
+export default function Navbar() {
   return (
-    <nav>
+    <Nav>
       <MenuItems className="container">
         {items.map((item, index) => (
           <StyledLink to="/order" id={item.id} key={index}>
@@ -83,6 +87,6 @@ export default function Navbar() {
           </StyledLink>
         ))}
       </MenuItems>
-    </nav>
+    </Nav>
   );
 }
