ece1a5b4c26b38ec85e1459095834ed3f27539c3
irfansubasi <136987051+irfansubasi@users.noreply.github.com>
2024-10-08 17:07:12 +0300

    add Home Page

---
+++ src/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,11 @@
+import MainHeader from '../../Components/MainHeader/MainHeader';
+import Navbar from '../../Components/Navbar/Navbar';
+
+export default function HomePage() {
+  return (
+    <>
+      <MainHeader />
+      <Navbar />
+    </>
+  );
+}
